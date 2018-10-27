24668c23a97777030ed393eae5956502203c3cf8
“zhanyun.huang” <zhanyun.huang@bgmyc.com>
2018-10-27 09:43:20 +0800

    完成支付回调

---
+++ public_html/callback/success.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Function:  支付成功回调
+ * User: YUN
+ * Date: 2018/10/27 09:20
+ * Description:
+ */
+
+$config = include 'config.php';
+
+$data = $_POST['data'];
+
+$pub_key = openssl_pkey_get_public($config['public_key']);
+$data_stream = base64_decode($data);
+openssl_public_decrypt($data_stream, $decrypt, $pub_key);
+$result = json_decode( $decrypt, true );
+echo '<pre>';
+print_r($result);
+print_r($_POST);
+
--- public_html/test_pay.php
+++ public_html/test_pay.php
@@ -1,31 +1,51 @@
 <?php
-$public_key = '-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArs825bTjoMRyEI19fDBP
-r4SVdxIATatgwHSlz5gXT5H2QmofN2x1nal7BKO9G244XKKeYOaZH4KIrxLT+beU
-FR0zzaQDBfh1N5aUKnn3tFsfVkZqbOXHMSUExdtNbijBZ53KFHQJz2ql1M7XzkhK
-gwdFWEI5tNAd0tX7F5E39d1INzl+Vo3R31piLCPwrKegdjkmhFbHJ4+ZFiECjmQ5
-r4pxQYxJWxZzU7xSOLeYLWIjLErTDn+gQQ7X+PqK7+O6XG/GQ+MXPjlPjnLIihiR
-5Rr58Tn0YjNoYgpvJ4Rm4D211TlyoHdzYlNgUwbk/EXFpOEOyFR5DYhmjNhjCmGs
-QwIDAQAB
------END PUBLIC KEY-----';
 
 $data = [
-    'order_id' => date('YmdHis'),
+    'order_id' => '201810260011',
     'amount' => '1.00',
 ];
 
-$public_key = file_get_contents('../includes/public.key');
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $public_key = file_get_contents('../includes/public.key');
+    $public_key_res = openssl_pkey_get_public($public_key);
+    $data_json = json_encode($data);
+    openssl_public_encrypt($data_json, $encrypt, $public_key_res);
+    $pay_data = urlencode(base64_encode($encrypt));
 
-$public_key_res = openssl_pkey_get_public($public_key);
-$data_json = json_encode($data);
-openssl_public_encrypt($data_json, $encrypt, $public_key_res);
-$pay_data = urlencode(base64_encode($encrypt));
+    header('Content-Type:application/json; charset=utf-8');
+    echo json_encode([
+        'success' => true,
+        'payment_data' => $pay_data
+    ]);
+    exit;
+}
+
+
 ?>
 <html>
 <meta charset="utf-8">
-<link href="https://cdn.bootcss.com/jqueryui/1.12.1/jquery-ui.css" rel="stylesheet">
+
+<script src="http://libs.baidu.com/jquery/2.1.1/jquery.min.js"></script>
+
 <body>
-    <a href="pay.php?payment_data=<?php echo $pay_data;?>">支付订单</a>
+<!--    <a href="pay.php?payment_data=--><?php //echo $pay_data;?><!--">GET 支付订单</a><br>-->
+    <a href="javascript:;" onclick="toPay('<?php echo $data['order_id']?>')">POST 支付订单</a>
+<script>
+    function toPay(order_id) {
+        $.ajax({
+            'type': 'POST',
+            'async': true,
+            'data': 'order_id=' + order_id,
+            'dataType': 'json',
+            'success': function(resp) {
+                if (resp.success) {
+//                    window.open('pay.php?payment_data=' + resp.payment_data);
+                    location.href = 'pay.php?payment_data=' + resp.payment_data;
+                }
+            }
+        });
+    }
+</script>
 </body>
 </html>
 
